34875c1754fee9eda6e755b1321b4c1d707c40dd
Alexander Zigelski <alexander.zigelski@obi.de>
2020-10-08 16:52:55 +0200

    simplify setup

--- kubernetes-drivers-license-1/1-first-scenario/install-kind.sh
+++ kubernetes-drivers-license-1/1-first-scenario/install-kind.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=stable bash)" && export PATH=$PATH:$HOME/go/bin
 go get -u -v sigs.k8s.io/kind
 
