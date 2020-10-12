12477e45a81081eec1e0bab953c14d92aec844c8
Alexander Zigelski <alexander.zigelski@obi.de>
2020-10-12 16:37:47 +0200

    simplified kind setup

--- kubernetes-drivers-license-1/kind-setup/install-kind.sh
+++ kubernetes-drivers-license-1/kind-setup/install-kind.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install kubectl
-snap install kubectl --classic
+curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.9/bin/linux/amd64/kubectl
+chmod +x /usr/local/bin/kubectl 
 
 # Install kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
-chmod +x ./kind
-mv kind /usr/local/bin
+curl -Lo /usr/local/bin/kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+chmod +x /usr/local/bin/kind
